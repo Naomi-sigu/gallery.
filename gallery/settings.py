ebe4f2d520616ea612251877f032c286d013bbd0
naomi <siguneema@gmail.com>
2019-10-09 16:40:15 +0300

    deploy to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-SECRET_KEY=config(SECRET_KEY)
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
