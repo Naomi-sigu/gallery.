67819466018a7d2a4aa08fb353457c1f165a7852
naomi <siguneema@gmail.com>
2019-10-11 14:26:17 +0300

    create the search function for the search bar to happen

--- photos/views.py
+++ photos/views.py
@@ -6,3 +6,17 @@ from .models import Image
 def welcome(request):
      images = Image.get_images()
      return render(request, 'home.html', {"images":images})
+
+     
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request,'search.html',{"message":message, "images":searched_images, "category":search_term})
+    
+    else:
+        message = "You haven't searched for any category"
+        
+        return render(request, 'search.html', {"message":message})
